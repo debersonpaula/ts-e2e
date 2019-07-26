ba49050dea29b586e98b2a2148d400ceb386ceb1
Deberson Paula <deberson.paula@gmail.com>
2019-07-26 10:06:27 -0300

    update deploy scripts

--- deploy.js
+++ deploy.js
@@ -3,19 +3,24 @@ const path = require('path');
 const copyFiles = require('./helpers/copyFiles');
 const createDir = require('./helpers/createDir');
 
-// !fs.existsSync('./deploy') && fs.mkdirSync('./deploy');
-createDir('./deploy');
-copyFiles(['./tmp/**/*', './deploy']).then(() => {
-  console.log('Copy source files successfully!');
-  copyRoot('tsconfig.json');
-  copyRoot('tslint.json');
-  copyRoot('readme.md');
-  copyRoot('package.json');
-}).catch(err => console.log(err));
+copyFilesBeforeDeploy();
 
+async function copyFilesBeforeDeploy() {
+  try {
+    createDir('./deploy');
+    await copyFiles(['./src/**/*.js', './deploy']);
+    await copyFiles(['./tmp/**/*', './deploy']);
+    copyRoot('tsconfig.json');
+    copyRoot('tslint.json');
+    copyRoot('readme.md');
+    copyRoot('package.json');
+    console.log('Copy source files successfully!');
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function copyRoot(name) {
   fs.copyFileSync(path.join(__dirname, name), path.join(__dirname, `./deploy/${name}`));
   console.log(`Copy file ${name} successfully!`);
 }
-
--- playground.js
+++ playground.js
@@ -3,23 +3,6 @@ const copyFiles = require('./helpers/copyFiles');
 
 copyFilesBeforeTesting();
 
-// copyfiles(['./playground/**/*.feature', './tmp'], { up: 1, verbose: true }, err => {
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log('Copy feature files finished!');
-// });
-
-// copyfiles(['./lib/**/*.js', './tmp/lib'], { up: 1, verbose: true }, err => {
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log('Copy js files finished!');
-// });
-
-// fs.mkdirSync('./tmp/results');
-// fs.writeFileSync('./tmp/results/output_result.json', '{}');
-
 async function copyFilesBeforeTesting() {
   await copyFiles(['./playground/**/*.feature', './tmp']);
   await copyFiles(['./playground/**/*.feature', './tmp/playground']);
